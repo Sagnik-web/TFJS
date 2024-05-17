6207c1e693924fc86e9072566d850aa908e7ad56
Sagnik Biswas <clicksagnikbiswas01@gmail.com>
2024-05-17 20:56:30 +0530

    tf.Tensor 3d shape  datatype

--- TF_Learn/01_Basic_TF/01_Matrix.js
+++ TF_Learn/01_Basic_TF/01_Matrix.js
@@ -19,7 +19,27 @@ const dimention1D = tf.tensor([12,15,16,19])
 
 
 const dimention2D = tf.tensor([12,15,16,17],[2,2],'int32')
-dimention2D.print()
+// dimention2D.print()
+// console.log(dimention2D);
+
+
+const dimention3D  = tf.tensor([[[12,14],[19,16]],[[10,40],[90,66]]])
+console.log(dimention3D);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
