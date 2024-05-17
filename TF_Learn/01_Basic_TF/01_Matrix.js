4172cab6be160042d53641b90dec3a0abf20e3f5
Sagnik Biswas <clicksagnikbiswas01@gmail.com>
2024-05-17 20:00:30 +0530

    tf.Tensor use

--- TF_Learn/01_Basic_TF/01_Matrix.js
+++ TF_Learn/01_Basic_TF/01_Matrix.js
@@ -2,10 +2,16 @@ const matrix_01 =()=>{
 
 const tf = require('@tensorflow/tfjs')
 
+
+// tf.tensor (values, shape?, dtype?) 
 const dimention_0 = tf.tensor(1)
 
-dimention_0.print()
+// dimention_0.print()
 // console.log(dimention_0);
+console.log(dimention_0.shape);
+console.log(dimention_0.size)
+console.log(dimention_0.dtype)
+
 
 }
 
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const matrix_01 = require("./TF_Learn/01_Basic_TF/01_matrix");
+const matrix_01 = require("./TF_Learn/01_Basic_TF/01_Matrix");
 
 
 matrix_01()
