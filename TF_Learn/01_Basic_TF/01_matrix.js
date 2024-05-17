6504bd68ddf48cd2f713d22523ae14d3b6239b91
Sagnik Biswas <clicksagnikbiswas01@gmail.com>
2024-05-17 19:51:38 +0530

    TF Learn Setup, Zero Dimention Tensor

---
+++ TF_Learn/01_Basic_TF/01_matrix.js
@@ -0,0 +1,12 @@
+const matrix_01 =()=>{
+
+const tf = require('@tensorflow/tfjs')
+
+const dimention_0 = tf.tensor(1)
+
+dimention_0.print()
+// console.log(dimention_0);
+
+}
+
+module.exports = matrix_01
---
+++ main.js
@@ -0,0 +1,4 @@
+const matrix_01 = require("./TF_Learn/01_Basic_TF/01_matrix");
+
+
+matrix_01()
