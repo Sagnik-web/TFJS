a546dfa9c45e778edb4ee5d21bf4ea09773430ce
Sagnik Biswas <clicksagnikbiswas01@gmail.com>
2024-05-17 21:16:09 +0530

    tf.Cast Change Data Type

---
+++ TF_Learn/01_Basic_TF/02_Covertion.js
@@ -0,0 +1,47 @@
+const connvertion_01 =()=>{
+    const tf = require('@tensorflow/tfjs')
+
+
+    const data = tf.tensor([1,2,3,4,5,0],[3,2])
+
+    const intCast = tf.cast(data,'int32')
+    // console.log(intCast);
+
+
+    const complexCast = tf.cast(data,'complex64')
+    // complexCast.print()
+    // console.log(complexCast);
+
+
+    const boolCast = tf.cast(data,'bool')
+    // boolCast.print()
+    // console.log(boolCast);
+
+
+    const changeBoolCast = tf.cast(boolCast,'int32')
+    // changeBoolCast.print()
+
+
+
+
+
+
+    const strVal = tf.tensor(['a','b','c'])
+    strVal.print()
+    // console.log(tf.cast(strVal,'bool')); XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}
+module.exports = connvertion_01
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const matrix_01 = require("./TF_Learn/01_Basic_TF/01_Matrix");
+const connvertion_01 = require("./TF_Learn/01_Basic_TF/02_Covertion");
 
 
-matrix_01()
+// matrix_01()
+
+connvertion_01()
